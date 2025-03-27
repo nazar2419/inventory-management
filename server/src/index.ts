15b26a0c38cdb28237dc6b2abe531fe2411274ab
Nazar <nazarx095@gmail.com>
2025-03-27 17:13:42 +0200

    products page

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,6 +6,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 /* ROUTE IMPORTS */
 import dashboardRoutes from "./routes/dashboardRoutes";
+import productRoutes from "./routes/productRoutes";
 /* CONFIGURATIONS */
 dotenv.config();
 const app = express();
@@ -18,6 +19,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 /* ROUTES */
 app.use("/dashboard", dashboardRoutes);
+app.use("/products", productRoutes);
 /* SERVER */
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
